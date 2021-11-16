97313142e6150c1dc80fa4ba71bca00ec8d491cb
Jeysson <jseclen@medialabla.com>
2021-11-15 19:56:32 -0500

    login css

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/login.css">
+    <title>Astromedic | Login</title>
+</head>
+<body>
+    <section class="login">
+        <div class="contenedor">
+            <h2>Inicio Sesión Astromedic</h2>
+            <form>
+                <input type="text" name="dni" id="dni" placeholder="Ingrese su DNI">
+                <input type="password" name="pass" id="pass" placeholder="Ingrese su contraseña">
+                <input type="submit" value="Iniciar sesión">
+            </form>
+        </div>
+    </section>
+    <footer>
+        <p>© 2021 Astromedic</p>
+    </footer>
+</body>
+
+</html>
